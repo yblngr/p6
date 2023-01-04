57e720e706f2b5fbd9bb73ac36a9604c8cde18aa
Yvan Boulanger <yvan.boulanger@gmail.com>
2023-01-04 12:00:15 +0100

    Version initiale

---
+++ srv/controllers/user.js
@@ -0,0 +1,40 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(() => res.status(201).json({ message: 'User created' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        res.status(401).json({ message: 'Wrong email or password' })
+      } else {
+        bcrypt.compare(req.body.password, user.password)
+          .then(valid => {
+            if (!valid) {
+              res.status(401).json({ message: 'Wrong email or password' })
+            } else {
+              res.status(200).json({
+                userId: user._id,
+                token: jwt.sign({ userId: user._id }, 'PIIQUANTE_TOKEN_SECRET', { expiresIn: '12h' })
+              });
+            }
+          })
+          .catch(error => res.status(500).json({ error }));
+      }
+    })
+    .catch(error => res.status(500).json({ error }));
+};
